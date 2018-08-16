e40694388324203871d82fd22973bb93643507f3
cmartin <martin.caleb.j@gmail.com>
2018-08-16 15:54:21 -0400

    implemented proper moves

--- pentago/PentagoLogic.py
+++ pentago/PentagoLogic.py
@@ -1,80 +1,109 @@
 from collections import namedtuple
+from itertools import product
 import numpy as np
+
+import PentagoUtilities as util
 
 DEFAULT_QUADRANT_SIZE = 3
 DEFAULT_WIN_LENGTH = 5
 
+Q = ['A','B','C','D']
+D = ['+','-']
+Qn = dict(zip(Q,[0,1,2,3]))
+Dn = dict(zip(D,[-1,1]))
+		
 WinState = namedtuple('WinState', 'is_ended winner')
 
 class Board():
-    """
-    Pentago Board.
-    """
+	"""
+	Pentago Board.
+	"""
 
-    def __init__(self, quadrant_size=None, win_length=None, np_pieces=None):
-        "Set up initial board configuration."
-        self.quadrant_size = quadrant_size or DEFAULT_QUADRANT_SIZE
+	def __init__(self, quadrant_size=None, win_length=None, np_pieces=None):
+		"Set up initial board configuration."
+		self.quadrant_size = quadrant_size or DEFAULT_QUADRANT_SIZE
 		self.height = self.quadrant_size*2
 		self.width = self.quadrant_size*2
-        self.win_length = win_length or DEFAULT_WIN_LENGTH
+		self.win_length = win_length or DEFAULT_WIN_LENGTH
+		
+		P = list(range(1,1+self.quadrant_size**2))
+		self.move_names = [''.join(map(str,m)) for m in product(Q,P,Q,D)]
 
-        if np_pieces is None:
-            self.np_pieces = np.zeros([self.height, self.width])
-        else:
-            self.np_pieces = np_pieces
-            assert self.np_pieces.shape == (self.height, self.width)
+		move_coords = []
+		dx = [0,0,self.quadrant_size,self.quadrant_size]
+		dy = [0,self.quadrant_size,0,self.quadrant_size]
+		for move in self.move_names:
+			x,y = np.unravel_index(int(move[1])-1, (self.quadrant_size, self.quadrant_size))
+			move_coords.append((x+dx[Qn[move[0]]], y+dy[Qn[move[0]]]))
+		self.move_coords = move_coords
 
-    def add_stone(self, x,y, player): # replace with execute_move (a2a+, c7b-, etc)
-        "Create copy of board containing new stone."
-        available_x,available_y = np.where(self.np_pieces == 0)
-        if len(available_x) == 0:
-            raise ValueError("Can't play position (%s,%s) on board %s" % (x,y, self))
+		if np_pieces is None:
+			self.np_pieces = np.zeros([self.height, self.width])
+		else:
+			self.np_pieces = np_pieces
+			assert self.np_pieces.shape == (self.height, self.width)
+		
+	def execute_move(self, move, player):
+		"Move string format is 'qXqD', where q=[A,B,C,D], X=keypad(1-9), D=+/-"
+		x,y = np.unravel_index(int(move[1])-1, (self.quadrant_size, self.quadrant_size))
+		move_q = Qn[move[0].upper()]
+		turn_q = Qn[move[2].upper()]
+		
+		upper_half = np.hsplit(np.vsplit(self.np_pieces, 2)[0], 2)
+		lower_half = np.hsplit(np.vsplit(self.np_pieces, 2)[1], 2)
+		quads = np.concatenate([upper_half, lower_half])
+		if quads[move_q][x][y] != 0:
+			raise ValueError("Can't play (Row %d, Col %d) in quadrant %s" % (x,y,move[0].upper()))
+		quads[move_q][x][y] = player
+		quads[turn_q] = np.rot90(quads[turn_q], Dn[move[3]])
+		upper_half = np.hstack([quads[0],quads[1]])
+		lower_half = np.hstack([quads[2],quads[3]])
+		self.np_pieces = np.vstack([upper_half, lower_half])
 
-        self.np_pieces[x][y] = player
+	def get_valid_moves(self):
+		"Any zero value is a valid move"
+		valid = [m for (m,xy) in zip(self.move_names, self.move_coords) if self.np_pieces[xy]==0]
+		return [self.np_pieces[xy] == 0 for xy in self.move_coords]
 
-    def get_valid_moves(self): # returns binary array of positions, not full moves
-        "Any zero value is a valid move"
-        return self.np_pieces == 0
+	def get_win_state(self):
+		for player in [-1, 1]:
+			player_pieces = self.np_pieces == -player
+			# Check rows & columns for win
+			if (self._is_straight_winner(player_pieces) or
+				self._is_straight_winner(player_pieces.transpose()) or
+				self._is_diagonal_winner(player_pieces)):
+				return WinState(True, -player)
 
-    def get_win_state(self):
-        for player in [-1, 1]:
-            player_pieces = self.np_pieces == -player
-            # Check rows & columns for win
-            if (self._is_straight_winner(player_pieces) or
-                self._is_straight_winner(player_pieces.transpose()) or
-                self._is_diagonal_winner(player_pieces)):
-                return WinState(True, -player)
+		# draw has very little value.
+		if not any(self.get_valid_moves()):
+			return WinState(True, None)
 
-        # draw has very little value.
-        if not self.get_valid_moves().any():
-            return WinState(True, None)
+		# Game is not ended yet.
+		return WinState(False, None)
 
-        # Game is not ended yet.
-        return WinState(False, None)
+	def with_np_pieces(self, np_pieces):
+		"""Create copy of board with specified pieces."""
+		if np_pieces is None:
+			np_pieces = self.np_pieces
+		return Board(self.quadrant_size, self.win_length, np_pieces)
 
-    def with_np_pieces(self, np_pieces):
-        """Create copy of board with specified pieces."""
-        if np_pieces is None:
-            np_pieces = self.np_pieces
-        return Board(self.height, self.width, self.win_length, np_pieces)
+	def _is_diagonal_winner(self, player_pieces):
+		"""Checks if player_pieces contains a diagonal win."""
+		win_length = self.win_length
+		for i in range(len(player_pieces) - win_length + 1):
+			for j in range(len(player_pieces[0]) - win_length + 1):
+				if all(player_pieces[i + x][j + x] for x in range(win_length)):
+					return True
+			for j in range(win_length - 1, len(player_pieces[0])):
+				if all(player_pieces[i + x][j - x] for x in range(win_length)):
+					return True
+		return False
 
-    def _is_diagonal_winner(self, player_pieces):
-        """Checks if player_pieces contains a diagonal win."""
-        win_length = self.win_length
-        for i in range(len(player_pieces) - win_length + 1):
-            for j in range(len(player_pieces[0]) - win_length + 1):
-                if all(player_pieces[i + x][j + x] for x in range(win_length)):
-                    return True
-            for j in range(win_length - 1, len(player_pieces[0])):
-                if all(player_pieces[i + x][j - x] for x in range(win_length)):
-                    return True
-        return False
+	def _is_straight_winner(self, player_pieces):
+		"""Checks if player_pieces contains a vertical or horizontal win."""
+		run_lengths = [player_pieces[:, i:i + self.win_length].sum(axis=1)
+					   for i in range(len(player_pieces) - self.win_length + 2)]
+		return max([x.max() for x in run_lengths]) >= self.win_length
 
-    def _is_straight_winner(self, player_pieces):
-        """Checks if player_pieces contains a vertical or horizontal win."""
-        run_lengths = [player_pieces[:, i:i + self.win_length].sum(axis=1)
-                       for i in range(len(player_pieces) - self.win_length + 2)]
-        return max([x.max() for x in run_lengths]) >= self.win_length
-
-    def __str__(self):
-        return str(self.np_pieces)
+	def __str__(self):
+		return str(self.np_pieces)
